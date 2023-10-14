9cd0088c0b5dab840635d3d80d79e06035db58b5
hayyaoe <hayya947@gmail.com>
2023-10-14 22:03:19 +0700

    finished

--- app/src/main/java/com/hayyaoe/guessthenumber/viewmodel/GPACounterViewModel.kt
+++ app/src/main/java/com/hayyaoe/guessthenumber/viewmodel/GPACounterViewModel.kt
@@ -4,11 +4,13 @@ import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.core.text.isDigitsOnly
 import androidx.lifecycle.ViewModel
 import com.hayyaoe.guessthenumber.model.GPAUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import java.text.DecimalFormat
 
 //sealed class ListGPAUiState{
 //    data class Loading(val data: MutableList<GPAUiState>) : ListGPAUiState()
@@ -23,14 +25,12 @@ class GPACounterViewModel: ViewModel() {
         val currentData = _uiState.value
 
         val newGPAUiState = GPAUiState(
-            score = score.toDouble(),
+            score = decimalFormat(score.toDouble()).toDouble(),
             sks = sks.toInt(),
             courseName = courseName
         )
 
         _uiState.value = currentData + newGPAUiState
-
-        Log.d("CEK", "${_uiState.value}")
     }
 
     fun removeScore(gpaUiState: GPAUiState){
@@ -40,11 +40,74 @@ class GPACounterViewModel: ViewModel() {
 
     fun getSks(): Int{
 
-        return 0
+        var sks = 0
+        val currentData = _uiState.value
+        for (gpa in currentData){
+            sks += gpa.sks
+        }
+        return sks
     }
 
-    fun getGpa(): Double{
+    fun getGpa(): String{
 
-        return 0.0
+        var sum = 0.0
+        var sks = 0.0
+        val currentData = _uiState.value
+
+        return if (_uiState.value.isNotEmpty()){
+
+            for (gpa in currentData) {
+                sum += (gpa.sks.toDouble() * gpa.score)
+                sks += gpa.sks
+            }
+            decimalFormat(sum / sks)
+        }else {
+            "0.0"
+        }
     }
+
+    private fun decimalFormat(number: Double): String{
+        val format = DecimalFormat("#,###0.00")
+        return format.format(number)
+    }
+
+    fun lessThanFour(input: String):Boolean{
+        return if (input.isNotBlank()&& inputDouble(input)) {
+            (input.toDouble() >4.0 || input.toDouble()<0.0)
+        }else{
+            false
+        }
+    }
+
+    private fun lessThanTwentyFour(input: String):Boolean{
+        return if (input.isNotBlank()) {
+            (input.toDouble() >24.0 || input.toDouble()<1.0)
+        }else{
+            false
+        }
+    }
+
+    private fun inputDouble(input: String): Boolean {
+        val regex = Regex("^-?\\d+(\\.\\d+)?$")
+        return regex.matches(input)
+    }
+
+    fun inputInteger(input: String): Boolean {
+        return if(input.isNotBlank()){
+            try {
+                val intValue = input.toInt()
+                intValue in 1..24
+            } catch (e: NumberFormatException) {
+                false
+            }
+        }else{
+            true
+        }
+
+    }
+
+    fun inputValid(score: String, sks : String, courseName: String): Boolean {
+        return score.isNotBlank() && sks.isNotBlank() && courseName.isNotBlank() && inputDouble(score) && inputInteger(sks) && !lessThanFour(score) && !lessThanTwentyFour(sks)
+    }
+
 }
